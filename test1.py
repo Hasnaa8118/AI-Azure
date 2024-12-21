797399f7794cd81920e1b30e3adc3a3256f26309
Hasnaa8118 <hasnaa.eddarif@gmail.com>
2024-12-21 15:30:50 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,4 +4,4 @@ st.title("EMC2/ Cloud Module")
 st.header("App for testing AI APIs")
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 st.sidebar.header("Master Cloud Computing")
-st.sidebar.selectbox('Select type of application', ['---choose application', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis']
+st.sidebar.selectbox('Select type of application', ['---choose application', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis'])
