372cf32a81252ff4ff24d609362fa3a7a9e116c3
Hasnaa8118 <hasnaa.eddarif@gmail.com>
2024-12-21 15:36:13 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.title("EMC2/ Cloud Module")
 st.header("App for testing AI APIs")
+
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 st.sidebar.header("Master Cloud Computing")
-st.sidebar.selectbox('Select type of application', ['---choose application', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis'])
+
+choix = st.sidebar.selectbox('Select type of application', ['---choose application', 'Image Analysis', 'OCR', 'Thumbnail Image', 'Face Analysis'])
+if choix == 'Image Analysis':
+  image_file = st.file_uploader('Load an image', type = ['png', 'jpg'])
