1c735b778a003e894894dbdece5791349d388e45
Hasnaa8118 <hasnaa.eddarif@gmail.com>
2024-12-21 15:14:06 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.write("Hello")
 st.write("My first azure app")
 
+st.title("This is the app title")
+st.header("This is the header")
+st.markdown("This is the markdown")
+st.subheader("This is the subheader")
+st.caption("This is the caption")
+st.code("x = 2021")
+st.latex(r" a+a r^1+a r^2+a r^3)
