dd4efc348d351a60f7350b81a7b3ef9f41c3b12c
Hasnaa8118 <hasnaa.eddarif@gmail.com>
2024-12-21 15:23:29 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("EMC2/ Cloud Module")
 st.header("App for testing AI APIs")
-st.sidebar.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fseeklogo.com%2Fvector-logo%2F451928%2Fecole-hassania-des-travaux-publics-ehtp&psig=AOvVaw3L3T0dJH6yiCp1TPFZptaQ&ust=1734877216738000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjaq7OHuYoDFQAAAAAdAAAAABAE")
-st.sidebar.header("Cloud Computing")
+st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
+st.sidebar.header("Master Cloud Computing")
